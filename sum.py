489b97806a0505da857ef5dc9ad3da85ad4a0feb
kmhellan <kmhellanc3a@gmail.com>
2014-12-22 16:21:14 +0530

    sum.py

---
+++ sum.py
@@ -0,0 +1,10 @@
+#sum of two numbers 
+num1  = input("Enter number 1:")
+num2 = input("Enter number 2:")
+sum = num1+num2
+print "sum=",sum
+#printing another way
+print "%d+%d=%d"%(num1,num2,sum)
+# sum in single step 
+
+print input("Enter num1:")+input("Enter num2:")
